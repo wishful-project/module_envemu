502f560f78de2e0f751a1f84e784d624529b4e0f
Pieter De Mil <pieter@demil.be>
2018-04-19 06:49:25 -0700

    Update and fix

--- wishful_module_envemu/module_envemu.py
+++ wishful_module_envemu/module_envemu.py
@@ -22,8 +22,8 @@ class EnvEmuModule(wishful_module.AgentModule):
         except FileNotFoundError:
             ee_node_output = ""
         if "/dev/ee" in ee_node_output:
-            self.ee_sf_process = subprocess.Popen(['../../experimentation_tools/EnvEmu/connectEE.rb'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-            self.ee_write_process = subprocess.Popen(['../../experimentation_tools/EnvEmu/write_to_ee.rb'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+            self.ee_sf_process = subprocess.Popen(['../../experimentation_tools/EnvEmu/connectEE.rb'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
+            self.ee_write_process = subprocess.Popen(['../../experimentation_tools/EnvEmu/write_to_ee.rb'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, shell=True)
             self.ee_connected = True
             self.__ee_thread_stop = threading.Event()
             self.__ee_thread = threading.Thread(target=self.__ee_listen, args=(self.__ee_thread_stop,))
@@ -55,7 +55,9 @@ class EnvEmuModule(wishful_module.AgentModule):
 
     def __ee_listen(self, stop_event):
         while not stop_event.is_set():
-            self.log.info("%s", self.ee_sf_process.stdout.readline().strip())
+            out = self.ee_sf_process.stdout.readline().strip()
+            if out != "":
+                self.log.info("%s", out)
 
     def before_init_energy_harvester(self):
         self.log.info("This function is executed before init_energy_harvester".format())
@@ -69,11 +71,11 @@ class EnvEmuModule(wishful_module.AgentModule):
     def init_energy_harvester(self):
         self.log.info("EE Module init_energy_harvester".format())
         self.log.info("Sending init config".format())
-        init_cmd = "EE_MSG_CONFIG_SAMPLER{msgt_am=248, EEMsgConfigSampler_time_us=0, EEMsgConfigSampler_ADCid=4, EEMsgConfigSampler_sampleRate=0, EEMsgConfigSampler_triggerLevel=0, EEMsgConfigSampler_numberOfSamplesReq=0,  EEMsgConfigSampler_samplerReportIDType=0}"
+        init_cmd = "EE_MSG_CONFIG_SAMPLER{msgt_am=248, EEMsgConfigSampler_time_us=0, EEMsgConfigSampler_ADCid=4, EEMsgConfigSampler_sampleRate=0, EEMsgConfigSampler_triggerLevel=0, EEMsgConfigSampler_numberOfSamplesReq=0,  EEMsgConfigSampler_samplerReportIDType=0}\n"
         self.ee_write_process.stdin.write(init_cmd)
         self.ee_write_process.stdin.flush()
         self.log.info("Stopping streamer just in case".format())
-        streamer_off_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=0, EEMsgConfigStreamer_harvesterCapacitor=0, EEMsgConfigStreamer_harvester=0, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }"
+        streamer_off_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=0, EEMsgConfigStreamer_harvesterCapacitor=0, EEMsgConfigStreamer_harvester=0, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue=4095}\n"
         self.ee_write_process.stdin.write(streamer_off_cmd)
         self.ee_write_process.stdin.flush()
         return 0
@@ -82,15 +84,16 @@ class EnvEmuModule(wishful_module.AgentModule):
     def start_energy_harvester(self):
         self.log.info("EE Module start_energy_harvester".format())
         self.log.info("Disabling USB".format())
-        disable_usb_cmd = "EE_MSG_SET_GPIO_PIN_STATUS{msgt_am=252, EEMsgSetGPIOPinStatus_time_us=0, EEMsgSetGPIOPinStatus_gpioPinStatus=0x18000000}"
+        disable_usb_cmd = "EE_MSG_SET_GPIO_PIN_STATUS{msgt_am=252, EEMsgSetGPIOPinStatus_time_us=0, EEMsgSetGPIOPinStatus_gpioPinStatus=0x18000000}\n"
         self.ee_write_process.stdin.write(disable_usb_cmd)
         self.ee_write_process.stdin.flush()
         self.log.info("Start streamer".format())
-        streamer_on_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=3550, EEMsgConfigStreamer_harvesterCapacitor=500000, EEMsgConfigStreamer_harvester=0, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }"
+        #streamer_on_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=3550, EEMsgConfigStreamer_harvesterCapacitor=500000, EEMsgConfigStreamer_harvester=0, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }\n"
+        streamer_on_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=3550, EEMsgConfigStreamer_harvesterCapacitor=0, EEMsgConfigStreamer_harvester=0, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }\n"
         self.ee_write_process.stdin.write(streamer_on_cmd)
         self.ee_write_process.stdin.flush()
         self.log.info("Start sampler".format())
-        start_sampler_cmd = "EE_MSG_CONFIG_SAMPLER{msgt_am=248, EEMsgConfigSampler_time_us=0, EEMsgConfigSampler_ADCid=4, EEMsgConfigSampler_sampleRate=2000, EEMsgConfigSampler_triggerLevel=0, EEMsgConfigSampler_numberOfSamplesReq=64001,  EEMsgConfigSampler_samplerReportIDType=1}"
+        start_sampler_cmd = "EE_MSG_CONFIG_SAMPLER{msgt_am=248, EEMsgConfigSampler_time_us=0, EEMsgConfigSampler_ADCid=4, EEMsgConfigSampler_sampleRate=2000, EEMsgConfigSampler_triggerLevel=0, EEMsgConfigSampler_numberOfSamplesReq=64001,  EEMsgConfigSampler_samplerReportIDType=1}\n"
         self.ee_write_process.stdin.write(start_sampler_cmd)
         self.ee_write_process.stdin.flush()
         return 0
@@ -99,15 +102,15 @@ class EnvEmuModule(wishful_module.AgentModule):
     def stop_energy_harvester(self):
         self.log.info("EE Module stop_energy_harvester".format())
         self.log.info("Stop streamer".format())
-        stop_sampler_cmd = "EE_MSG_CONFIG_SAMPLER{msgt_am=248, EEMsgConfigSampler_time_us=0, EEMsgConfigSampler_ADCid=4, EEMsgConfigSampler_sampleRate=2000, EEMsgConfigSampler_triggerLevel=0, EEMsgConfigSampler_numberOfSamplesReq=0,  EEMsgConfigSampler_samplerReportIDType=1}"
+        stop_sampler_cmd = "EE_MSG_CONFIG_SAMPLER{msgt_am=248, EEMsgConfigSampler_time_us=0, EEMsgConfigSampler_ADCid=4, EEMsgConfigSampler_sampleRate=2000, EEMsgConfigSampler_triggerLevel=0, EEMsgConfigSampler_numberOfSamplesReq=0,  EEMsgConfigSampler_samplerReportIDType=1}\n"
         self.ee_write_process.stdin.write(stop_sampler_cmd)
         self.ee_write_process.stdin.flush()
         self.log.info("Stop streamer".format())
-        streamer_off_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=0, EEMsgConfigStreamer_harvesterCapacitor=0, EEMsgConfigStreamer_harvester=0, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }"
+        streamer_off_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=0, EEMsgConfigStreamer_harvesterCapacitor=0, EEMsgConfigStreamer_harvester=0, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }\n"
         self.ee_write_process.stdin.write(streamer_off_cmd)
         self.ee_write_process.stdin.flush()
         self.log.info("Enabling USB".format())
-        enable_usb_cmd = "EE_MSG_SET_GPIO_PIN_STATUS{msgt_am=252, EEMsgSetGPIOPinStatus_time_us=0, EEMsgSetGPIOPinStatus_gpioPinStatus=0x00001800}"
+        enable_usb_cmd = "EE_MSG_SET_GPIO_PIN_STATUS{msgt_am=252, EEMsgSetGPIOPinStatus_time_us=0, EEMsgSetGPIOPinStatus_gpioPinStatus=0x00001800}\n"
         self.ee_write_process.stdin.write(enable_usb_cmd)
         self.ee_write_process.stdin.flush()
         return 0
@@ -116,7 +119,7 @@ class EnvEmuModule(wishful_module.AgentModule):
     def update_energy_harvester(self):
         self.log.info("EE Module update_energy_harvester".format())
         self.log.info("Update streamer".format())
-        update_streamer_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=0, EEMsgConfigStreamer_harvesterCapacitor=500000, EEMsgConfigStreamer_harvester=10, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }"
+        update_streamer_cmd = "EE_MSG_CONFIG_STREAMER{msgt_am=250, EEMsgConfigStreamer_time_us=0, EEMsgConfigStreamer_DACid=1, EEMsgConfigStreamer_sample=0, EEMsgConfigStreamer_harvesterCapacitor=500000, EEMsgConfigStreamer_harvester=10, EEMsgConfigStreamer_outerLoopResistor=0, EEMsgConfigStreamer_maxDACvalue = 4095 }\n"
         self.ee_write_process.stdin.write(update_streamer_cmd)
         self.ee_write_process.stdin.flush()
         return 0
